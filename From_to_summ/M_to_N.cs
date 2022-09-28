f7076cb06f291ae5359e1a1695b1b368eb456264
Max <Max.Vinner@bk.ru>
2022-09-29 00:37:16 +0400

    create homework_sem_9

---
+++ From_to_summ/M_to_N.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int sum_from_to(int M, int N)
+{
+    int res = M;
+    if (M == N)
+        return 0;
+    else
+    {
+        return N+sum_from_to(M,N-1);
+    }
+}
+Console.WriteLine(sum_from_to(M,N));
---
+++ Int_downto_N/integer_line.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число: ");
+int count = Convert.ToInt32(Console.ReadLine());
+int finish =1;
+void Didgit_Lint(int finish, int count)
+{
+    if (count < finish)
+    {
+        return;
+    }
+    else
+    {
+        Didgit_Lint(finish, count  - 1);
+        Console.Write (count + " ");
+    }
+}
+try
+{
+    Didgit_Lint(finish,count);
+}
+catch 
+{
+    Console.WriteLine("oh...something wrong");
+}
